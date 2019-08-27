877c130faacd9c45a0ff1d5089aca2efec4fce97
Scott Young <youngscottj@gmail.com>
2019-08-26 19:34:41 -0600

    initialize crud actions for pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,20 @@
 class PagesController < ApplicationController
+  # view all pages
   def index
+    @pages = Page.all
+    # renders index.html.erb
   end
 
+  # shows a single object
   def show
+    @page = Page.find(params[:id])
+    # renders show.html.erb
   end
 
+  # displays form to create new page
   def new
+    @page = Page.new
+    # renders new.html.erb
   end
 
   def edit
